f12e4e3353df5ccee7e7e3ac4baeee6c382cb615
engineerknow <mycomengineer@gmail.com>
2024-05-31 14:17:14 +0700

    yr

--- resources/views/groups/document-system/overtime/document/index.blade.php
+++ resources/views/groups/document-system/overtime/document/index.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
     <div>
         <div>
             <div class="container-fluid">
@@ -207,10 +205,6 @@
         {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> --}}
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
         <script>
             $(document).ready(function() {
                 $('.select2').select2()
