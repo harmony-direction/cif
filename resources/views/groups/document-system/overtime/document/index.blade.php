ba4a0162074d426a63d4c962347b06cb7fc144b7
engineerknow <mycomengineer@gmail.com>
2024-05-31 14:46:44 +0700

    CLEAN CODE

--- resources/views/groups/document-system/overtime/document/index.blade.php
+++ resources/views/groups/document-system/overtime/document/index.blade.php
@@ -186,10 +186,6 @@
 
         <script type="module" src="{{ asset('assets/js/helpers/document-system/overtime/document/index.js?v=4') }}"></script>
         <script src="{{ asset('assets/js/helpers/helper.js?v=1') }}"></script>
-                <!-- jQuery and Bootstrap JS -->
-        {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> --}}
         <script>
             $(document).ready(function() {
                 $('.select2').select2()
