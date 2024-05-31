a88292390fdbc0805949384c341481436f90353d
engineerknow <mycomengineer@gmail.com>
2024-05-31 11:58:53 +0700

    try

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -28,9 +28,17 @@ class ImageController extends Controller
         return Storage::disk('announcement-attachments')->download($file);
     }
     public function avatar_view($image) {
-        $path = storage_path('app/avatar/' . $image);
+        $path = 'uploads/attachments/' . $image;
+
+        if (!Storage::disk('attachments')->exists($path)) {
+            abort(404); // File not found
+        }
+
+        $file = Storage::disk('attachments')->get($path);
+        $type = Storage::disk('attachments')->mimeType($path);
+        /* $path = storage_path('app/avatar/' . $image);
         $file = \File::get($path);
-        $type = \File::mimeType($path);
+        $type = \File::mimeType($path); */
         $response = \Response::make($file, 200);
         $response->header('Content-Type', $type);
         return $response;
