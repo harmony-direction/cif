fa2f67104745b6bbb781a642b510976fa407175a
engineerknow <mycomengineer@gmail.com>
2024-05-31 14:14:38 +0700

    try

--- resources/views/groups/document-system/overtime/document/index.blade.php
+++ resources/views/groups/document-system/overtime/document/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.dashboard')
 
 @section('content')
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
     <div>
         <div>
             <div class="container-fluid">
@@ -110,6 +112,16 @@
                                       <a class="dropdown-item" href="#">Link 3</a>
                                     </div>
                                   </div>
+                                  <div class="dropdown mt-3">
+                                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                                      Dropdown button
+                                    </button>
+                                    <div class="dropdown-menu">
+                                      <a class="dropdown-item" href="#">Link 1</a>
+                                      <a class="dropdown-item" href="#">Link 2</a>
+                                      <a class="dropdown-item" href="#">Link 3</a>
+                                    </div>
+                                </div>
                             </ul>
 
                             </div>
@@ -195,6 +207,9 @@
         {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> --}}
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
         <script>
             $(document).ready(function() {
